190441f279278c5b58287279c4f3b56880263f4c
Harish12121 <harishmsece@gmail.com>
2024-04-04 01:09:30 +0530

    Added TypeScript file

---
+++ Portfolio-Website/Portfolio-Website-master/script.js
@@ -0,0 +1,47 @@
+//contact form to excel sheet
+const scriptURL = 'https://docs.google.com/spreadsheets/d/1Aa98LQP92KhYeijXILBiZNFp-GNVMv0AImHrrWm2RMM/edit#gid=0';
+const form = document.forms['submitToGoogleSheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => {
+      msg.innerHTML = "Message sent successfully"
+      setTimeout(function () {
+        msg.innerHTML = ""
+      }, 5000)
+      form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
+    
+  });
+
+function updateActiveSection() {
+  var scrollPosition = $(window).scrollTop();
+
+  // Checking if scroll position is at the top of the page
+  if (scrollPosition === 0) {
+    $(".header ul li a").removeClass("active");
+    $(".header ul li a[href='#home']").addClass("active");
+    return;
+  }
+
+  // Iterate through each section and update the active class in the header
+  $("section").each(function () {
+    var target = $(this).attr("id");
+    var offset = $(this).offset().top;
+    var height = $(this).outerHeight();
+
+    if (
+      scrollPosition >= offset - 40 &&
+      scrollPosition < offset + height - 40
+    ) {
+      $(".header ul li a").removeClass("active");
+      $(".header ul li a[href='#" + target + "']").addClass("active");
+    }
+  });
+}
+
+
