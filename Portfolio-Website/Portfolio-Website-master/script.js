cd73027fd4ca05393da0573ff095696deed6a7ca
Harish12121 <harishmsece@gmail.com>
2024-04-04 02:38:09 +0530

    spreadsheets connect to forms

--- Portfolio-Website/Portfolio-Website-master/script.js
+++ Portfolio-Website/Portfolio-Website-master/script.js
@@ -1,22 +1,4 @@
-//contact form to excel sheet
-const scriptURL = 'https://docs.google.com/spreadsheets/d/1Aa98LQP92KhYeijXILBiZNFp-GNVMv0AImHrrWm2RMM/edit#gid=0';
-const form = document.forms['submitToGoogleSheet']
-const msg = document.getElementById("msg")
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-    .then(response => {
-      msg.innerHTML = "Message sent successfully"
-      setTimeout(function () {
-        msg.innerHTML = ""
-      }, 5000)
-      form.reset()
-    })
-    .catch(error => console.error('Error!', error.message))
-})
-    
-  });
 
 function updateActiveSection() {
   var scrollPosition = $(window).scrollTop();
